00918b0149f0172cc0851505367309bbc362583f
muhammed Bilal <mdbilaal777@gmail.com>
2025-04-21 22:03:11 +0530

    setup test file

---
+++ src/index.test.ts
@@ -0,0 +1,11 @@
+// index.test.ts
+import request from "supertest";
+import app from ".";
+
+describe("GET /", () => {
+  it('responds with "Hello from Express + TypeScript!"', async () => {
+    const res = await request(app).get("/");
+    expect(res.status).toBe(200);
+    expect(res.text).toBe("Hello from Express + TypeScript!");
+  });
+});
